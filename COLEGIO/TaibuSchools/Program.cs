80d82454ba8330514eb79559e5f1c88c180b7226
amtaibu <isabiryetai645@gmail.com>
2022-02-10 23:54:41 +0300

    Designed the home page of the system administrator

--- COLEGIO/TaibuSchools/Program.cs
+++ COLEGIO/TaibuSchools/Program.cs
@@ -23,5 +23,8 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapControllerRoute(
+    name: "Student",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
